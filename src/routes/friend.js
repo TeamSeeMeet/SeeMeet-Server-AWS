b42494444c1926b6be6659475c54179e61fed2ed
namjiyun <namjiyun3275@gmail.com>
2022-05-10 16:25:40 +0900

    [Feat] friend 라우팅

--- src/routes/friend.js
+++ src/routes/friend.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const auth = require('../controllers/auth');
+const friend = require('../controllers/friend');
 const router = express.Router();
 
 router.post('/social', auth.authSocialLogin);
 router.put('/', auth.authSignup);
+
+router.get('/list', friend.getFriend);
+router.post('/search', friend.searchFriend);
+router.post('/addFriend', friend.addFriend);
+router.put('/block', friend.blockFriend);
+router.put('/cancelblock', friend.cancleBlockFriend);
 
 module.exports = router;

724bf81c3609bbb231f9b4b44d3c7e6cd3bba5a0
namjiyun <namjiyun3275@gmail.com>
2022-05-10 16:57:30 +0900

    [Feat] invitation 라우팅

--- src/routes/invitation.js
+++ src/routes/invitation.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const auth = require('../controllers/auth');
+const invitation = require('../controllers/invitation');
 const router = express.Router();
 
 router.post('/social', auth.authSocialLogin);
 router.put('/', auth.authSignup);
+
+router.get('/list', invitation.getInvitation);
+router.post('/', invitation.postInvitation);
+router.get('/:invitationId', invitation.getInvitationById);
+router.post('/:invitationId', invitation.confirmInvitation);
+router.put('/:invitationId', invitation.cancelInvitation);
+router.get('/canceled/:invitationId', invitation.getCanceledInvitation);
 
 module.exports = router;

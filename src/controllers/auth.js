a2ce20da570eb3686725f0ba442239951198e4d5
namjiyun <namjiyun3275@gmail.com>
2022-05-17 10:12:09 +0900

    [Fix]

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -98,67 +98,7 @@ const authSignup = async (req, res) => {
   }
 };
 
-const signup = async (req, res) => {
-  const { email, password, passwordConfirm } = req.body;
-  if (!email || !password || !passwordConfirm) {
-    await send(`email : ${email}`);
-    return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-  }
-  if (password.length < 8) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.PASSWORD_LENGTH_SHORT));
-  if (password != passwordConfirm) return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.PASSWORD_IS_NOT_CORRECT));
-
-  let client;
-  try {
-    client = await db.connect(req);
-    const userFirebase = await admin
-      .auth()
-      .createUser({ email, password })
-      .then(user => user)
-      .catch(e => {
-        console.log(e);
-        return { err: true, error: e };
-      });
-
-    if (userFirebase.err) {
-      if (userFirebase.error.code === 'auth/email-already-exists') {
-        const checkuser = await userService.returnUser(client, email);
-        if (!checkuser) {
-          return res.status(statusCode.NOT_FOUND).json(util.fail(statusCode.NOT_FOUND, '해당 이메일을 가진 유저가 이미 있습니다.'));
-        } else {
-          return res.status(statusCode.OK).json(util.success(statusCode.OK, '재가입하였습니다.'));
-        }
-      } else if (userFirebase.error.code === 'auth/invalid-password') {
-        return res.status(statusCode.NOT_FOUND).json(util.fail(statusCode.NOT_FOUND, ' 비밀번호 형식이 잘못되었습니다. 패스워드는 최소 6자리의 문자열이어야합니다.'));
-      } else if (userFirebase.error.code === 'auth/invalid-email') {
-        return res.status(statusCode.NOT_FOUND).json(util.fail(statusCode.NOT_FOUND, '이메일 형식이 올바르지 않습니다.'));
-      } else {
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).json(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-      }
-    }
-    const id_Firebase = userFirebase.uid;
-    const user = await userService.addUser(client, email, id_Firebase);
-    if (!user) {
-      await send(`email : ${email}`);
-      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-    }
-    const { accesstoken } = jwtHandlers.sign(user);
-    if (!accesstoken) {
-      await send(`accesstoken : ${accesstoken}`);
-      return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-    }
-
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.CREATED_USER, { user, accesstoken }));
-  } catch (error) {
-    console.log(error);
-    await send(error);
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
-  } finally {
-    client.release();
-  }
-};
-
 module.exports = {
   authSocialLogin,
   authSignup,
-  signup,
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,6 @@ const auth = require('../controllers/auth');
 const router = express.Router();
 
 router.post('/social', auth.authSocialLogin);
-router.post('/', auth.signup);
 router.put('/', auth.authSignup);
 
 module.exports = router;

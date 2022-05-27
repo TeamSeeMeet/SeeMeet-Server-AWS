d12844f2e297376c73a741326af07b54bc8c0e0e
namjiyun <namjiyun3275@gmail.com>
2022-05-27 14:49:18 +0900

    [Fix] 라우팅에러 수정

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,6 @@ router.use('/auth', require('./auth'));
 router.use('/friend', require('./friend'));
 router.use('/plan', require('./plan'));
 router.use('/invitation', require('./invitation'));
-router.use('/invitationResponse', require('./invitationResponse'));
+router.use('/invitation-response', require('./invitationResponse'));
 
 module.exports = router;

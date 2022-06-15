3a20de5eab18f5ce241dceab236df16aa8bb6956
namjiyun <namjiyun3275@gmail.com>
2022-06-15 16:36:28 +0900

    [Feat] 푸시알림 모듈 구현

---
+++ src/modules/pushAlarm.js
@@ -0,0 +1,60 @@
+const admin = require('firebase-admin');
+const util = require('./util');
+const statusCode = require('../modules/statusCode');
+const responseMessage = require('../modules/responseMessage');
+
+const sendPushAlarm = async (req, res, title, body, receiverToken) => {
+  //   let mutableContent = 1;
+
+  // FCM Token이 empty인 경우 제외
+  //   receiverToken = receiverToken.filter(t => t);
+  //   if (!receiverToken.length) {
+  //     return true;
+  //   }
+
+  try {
+    const message = {
+      //   data: {
+      //     title: title,
+      //     body: body,
+      //   },
+      //   tokens: receiverToken,
+      android: {
+        data: {
+          title,
+          body,
+        },
+      },
+      apns: {
+        payload: {
+          aps: {
+            alert: {
+              title,
+              body,
+            },
+          },
+        },
+      },
+      tokens: receiverToken,
+    };
+
+    admin
+      .messaging()
+      .sendMulticast(message)
+      .then(function (response) {
+        console.log('Successfully sent message: : ', response);
+        return res.status(200).json({ success: true });
+      })
+      .catch(function (err) {
+        console.log('Error Sending message!!! : ', err);
+        return res.status(400).json({ success: false });
+      });
+  } catch (error) {
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+  } finally {
+  }
+};
+
+module.exports = {
+  sendPushAlarm,
+};

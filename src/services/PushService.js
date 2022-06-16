fb621e07f2cf6864f314b0238df1453992f81e3b
namjiyun <namjiyun3275@gmail.com>
2022-06-16 13:50:13 +0900

    [Feat] 푸시알림 on/off 처리

--- src/services/PushService.js
+++ src/services/PushService.js
@@ -8,7 +8,7 @@ const pushPlan = async (client, today) => {
           SELECT u.fcm, plan.id
           FROM plan, invitation_date, plan_user_connection pu, "user" u
           WHERE plan.invitation_date_id = invitation_date.id AND invitation_date.date = $1
-          AND pu.plan_id = plan.id AND pu.user_id = u.id 
+          AND pu.plan_id = plan.id AND pu.user_id = u.id AND u.push = true
          
           `,
     [today],

3e93aeed89553d93cc484dac50d665bbf97f3723
namjiyun <namjiyun3275@gmail.com>
2022-06-23 09:46:55 +0900

    [Fix] db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,10 +1,27 @@
-const { Pool, Query } = require('pg'); //postgres
+const { Pool, Query } = require('pg');
 const dayjs = require('dayjs');
 const dotenv = require('dotenv');
+
 dotenv.config();
 
 const dbConfig = require('../config/index');
-const { send } = require('../modules/slack');
+
+let devMode = process.env.NODE_ENV === 'development';
+
+const sqlDebug = true;
+
+const submit = Query.prototype.submit;
+
+Query.prototype.submit = function () {
+  const text = this.text;
+  const values = this.values || [];
+  const query = text.replace(/\$([0-9]+)/g, (m, v) => JSON.stringify(values[parseInt(v) - 1]));
+
+  devMode && sqlDebug && console.log(`\n\n[👻 SQL STATEMENT]\n${query}\n_________\n`);
+  submit.apply(this, arguments);
+};
+
+console.log(`[🔥DB] ${process.env.NODE_ENV}`);
 
 const pool = new Pool({
   ...dbConfig,
@@ -20,16 +37,16 @@ const connect = async req => {
           !!req.params && `params ${JSON.stringify(req.params)}`
         }`
       : `request 없음`;
-  await send(string);
   const callStack = new Error().stack;
   const client = await pool.connect();
   const query = client.query;
   const release = client.release;
-  //const releaseChecker = setTimeout(() => {
-  //  devMode
+
+  // const releaseChecker = setTimeout(() => {
+  //   devMode
   //     ? console.error('[ERROR] client connection이 15초 동안 릴리즈되지 않았습니다.', { callStack })
-  //    : functions.logger.error('[ERROR] client connection이 15초 동안 릴리즈되지 않았습니다.', { callStack });
-  //  devMode ? console.error(`마지막으로 실행된 쿼리문입니다. ${client.lastQuery}`) : functions.logger.error(`마지막으로 실행된 쿼리문입니다. ${client.lastQuery}`);
+  //     : functions.logger.error('[ERROR] client connection이 15초 동안 릴리즈되지 않았습니다.', { callStack });
+  //   devMode ? console.error(`마지막으로 실행된 쿼리문입니다. ${client.lastQuery}`) : functions.logger.error(`마지막으로 실행된 쿼리문입니다. ${client.lastQuery}`);
   // }, 15 * 1000);
 
   client.query = (...args) => {
@@ -37,11 +54,11 @@ const connect = async req => {
     return query.apply(client, args);
   };
   client.release = () => {
-    //   clearTimeout(releaseChecker);
+    // clearTimeout(releaseChecker);
     const time = dayjs().diff(now, 'millisecond');
     if (time > 4000) {
       const message = `[RELEASE] in ${time} | ${string}`;
-      //  devMode && console.log(message);
+      devMode && console.log(message);
     }
     client.query = query;
     client.release = release;
@@ -50,6 +67,4 @@ const connect = async req => {
   return client;
 };
 
-module.exports = {
-  connect,
-};
+module.exports = { connect };
